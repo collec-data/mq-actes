34d35bf4b5a2ef753421f659bde2594ddb239099
gautierr <romain.gautier@sib.fr>
2022-10-21 17:45:48 +0200

    Experiments modules et page de debug

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subject } from 'rxjs';
+import { filter, takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'opendata-marqueblanche';
+  includeDebug = false
+
+  destroy$ = new Subject<boolean>();
+
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    
+    this.route.queryParams
+      .pipe(
+        filter(params => params['debug']),
+        takeUntil(this.destroy$)
+      )
+      .subscribe(_ => this.includeDebug = true)
+  }
+
+  ngOnDestroy(): void {
+      this.destroy$.next(true);
+      this.destroy$.unsubscribe();
+  }
 }
