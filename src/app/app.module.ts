4eecfa0ccfb09cd73f24270b1afcd2ab5ea02f20
gautierr <romain.gautier@sib.fr>
2022-10-25 10:05:57 +0200

    media queries dans la vue debug

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { LayoutModule } from '@angular/cdk/layout'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,11 +9,13 @@ import { DebugComponent } from './debug/debug.component';
 @NgModule({
   declarations: [
     AppComponent,
-    DebugComponent
+    DebugComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+    LayoutModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/debug/debug.component.ts
+++ src/app/debug/debug.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-debug',
   templateUrl: './debug.component.html',
   styleUrls: ['./debug.component.sass']
 })
-export class DebugComponent implements OnInit {
+export class DebugComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  destroy$ = new Subject<boolean>();
+
+  mediaQueriesState: [string,boolean][] = []
+
+  constructor(private responsive: BreakpointObserver) { }
+
+  ngOnDestroy(): void {
+      this.destroy$.next(true);
+      this.destroy$.unsubscribe();
+  }
 
   ngOnInit(): void {
+
+    this.setupBreakPointObservers();
+  }
+
+  setupBreakPointObservers() {
+    let breakpoints = [
+      Breakpoints.XSmall, Breakpoints.Small, Breakpoints.Medium, Breakpoints.Large, Breakpoints.XLarge,
+      Breakpoints.Handset, Breakpoints.Tablet, Breakpoints.Web,
+      Breakpoints.HandsetPortrait, Breakpoints.HandsetLandscape,
+      Breakpoints.TabletPortrait, Breakpoints.TabletLandscape,
+      Breakpoints.WebPortrait, Breakpoints.WebLandscape,
+    ];
+
+    this.responsive.observe(breakpoints)
+    .pipe(
+      takeUntil(this.destroy$)
+    )
+    .subscribe(r => {
+      this.mediaQueriesState = Object.entries(r.breakpoints);
+    })
   }
 
 }
