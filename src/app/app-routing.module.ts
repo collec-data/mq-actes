b85d13cb22a94e2ce2ec24921110bd26d49ab61f
gautierr <romain.gautier@sib.fr>
2022-10-26 10:51:48 +0200

    Fallback route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { IFrameRedirectVersMarqueBlancheGuard } from './iframe-redirect.guard';
 
+export const APPLICATION_PATH = 'a'
+export const APPLICATION_URL = `/${APPLICATION_PATH}`
 
 export const MARQUE_BLANCHE_PATH = 'marque-blanche'
 export const MARQUE_BLANCHE_URL = `/${MARQUE_BLANCHE_PATH}`
 
 const routes: Routes = [
   { path: '', redirectTo: 'a', pathMatch: 'full' },
-  { path: 'a', loadChildren: () => import('./principal/principal.module').then(m => m.PrincipalModule), canLoad: [IFrameRedirectVersMarqueBlancheGuard] },
+  { path: APPLICATION_PATH, loadChildren: () => import('./principal/principal.module').then(m => m.PrincipalModule), canLoad: [IFrameRedirectVersMarqueBlancheGuard] },
   { path: MARQUE_BLANCHE_PATH, loadChildren: () => import('./marque-blanche/marque-blanche.module').then(m => m.MarqueBlancheModule) },
-  { path: 'experimentations', loadChildren: () => import('./experimentations/experimentations.module').then(m => m.ExperimentationsModule) }
+  { path: 'experimentations', loadChildren: () => import('./experimentations/experimentations.module').then(m => m.ExperimentationsModule) },
+  { path: '**', redirectTo: APPLICATION_PATH },
 ];
 
 @NgModule({
