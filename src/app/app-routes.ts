57b022ac1fc832dfda4122e0e4df4f47262cb2be
Emanuel Guével <emanuel.guevel@sib.fr>
2022-10-28 11:42:19 +0200

    Renommage route: accueil

--- src/app/app-routes.ts
+++ src/app/app-routes.ts
@@ -1,16 +1,26 @@
 import { Routes } from '@angular/router';
 import { IFrameRedirectVersMarqueBlancheGuard } from './iframe-redirect.guard';
 
-export const APPLICATION_PATH = 'a'
-export const APPLICATION_URL = `/${APPLICATION_PATH}`
+export const ACCUEIL_PATH = 'accueil';
+export const ACCUEIL_URL = `/${ACCUEIL_PATH}`;
 
-export const MARQUE_BLANCHE_PATH = 'marque-blanche'
-export const MARQUE_BLANCHE_URL = `/${MARQUE_BLANCHE_PATH}`
+export const MARQUE_BLANCHE_PATH = 'marque-blanche';
+export const MARQUE_BLANCHE_URL = `/${MARQUE_BLANCHE_PATH}`;
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'a', pathMatch: 'full' },
-  { path: APPLICATION_PATH, loadChildren: () => import('./principal/principal-routes').then(m => m.routes), canLoad: [IFrameRedirectVersMarqueBlancheGuard] },
-  { path: MARQUE_BLANCHE_PATH, loadChildren: () => import('./marque-blanche/marque-blanche-routes').then(m => m.routes) },
-  { path: 'experimentations', loadChildren: () => import('./experimentations/experimentations-routes').then(m => m.routes) },
-  { path: '**', redirectTo: APPLICATION_PATH },
+  {path: '', redirectTo: ACCUEIL_PATH, pathMatch: 'full'},
+  {
+    path: ACCUEIL_PATH,
+    loadChildren: () => import('./principal/principal-routes').then(m => m.routes),
+    canLoad: [IFrameRedirectVersMarqueBlancheGuard]
+  },
+  {
+    path: MARQUE_BLANCHE_PATH,
+    loadChildren: () => import('./marque-blanche/marque-blanche-routes').then(m => m.routes)
+  },
+  {
+    path: 'experimentations',
+    loadChildren: () => import('./experimentations/experimentations-routes').then(m => m.routes)
+  },
+  {path: '**', redirectTo: ACCUEIL_PATH},
 ];
