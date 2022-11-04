5afef6b409680ea1067060dfb1d182d9ba7609ae
Emanuel Guével <emanuel.guevel@sib.fr>
2022-11-04 15:38:09 +0100

    Liens sur la page d'accueil

--- src/app/principal/components/accueil/accueil.component.ts
+++ src/app/principal/components/accueil/accueil.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterModule } from "@angular/router";
 
 @Component({
   selector: 'app-accueil',
   templateUrl: './accueil.component.html',
+  imports: [
+    RouterModule
+  ],
   standalone: true
 })
 export class AccueilComponent {
