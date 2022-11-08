a066e19bd9e13543580b72b17e3067076c0fba3d
Emanuel Guével <emanuel.guevel@sib.fr>
2022-11-08 14:44:04 +0100

    Ajout du composant marque blanche app-search-list (déplacement de code depuis app-accueil)

--- src/app/marque-blanche/components/accueil/accueil.component.ts
+++ src/app/marque-blanche/components/accueil/accueil.component.ts
@@ -1,60 +1,16 @@
-import { Component, inject, OnInit, ViewChild } from '@angular/core';
-import { SearchPanelComponent } from "../../../components/search-panel/search-panel.component";
-import {
-  PaginatedDocumentListComponent
-} from "../../../components/paginated-document-list/paginated-document-list.component";
-import { Pageable, SearchParams } from "../../../models/model";
-import { SearchService } from "../../../search.service";
-import { finalize, Subject, switchMap, tap } from "rxjs";
+import { Component } from '@angular/core';
+import { SearchListComponent } from "../search-list/search-list.component";
 
 @Component({
   selector: 'app-accueil',
   templateUrl: './accueil.component.html',
   standalone: true,
   imports: [
-    SearchPanelComponent,
-    PaginatedDocumentListComponent
+    SearchListComponent
   ],
   host: {
-    class: 'd-flex flex-column m-l'
+    class: 'd-flex'
   }
 })
-export class AccueilComponent implements OnInit {
-  private searchService = inject(SearchService);
-  private searchParams?: SearchParams;
-
-  @ViewChild(PaginatedDocumentListComponent) documentList!: PaginatedDocumentListComponent;
-  searchLaunched = false;
-  pageLoading = false;
-
-  pageLoad = new Subject<{ params: SearchParams, pageable?: Pageable }>();
-
-  ngOnInit() {
-    this.pageLoad.pipe(
-      tap(() => {
-        this.searchLaunched = true;
-      }),
-      switchMap(({params, pageable}) => {
-        this.pageLoading = true;
-        return this.searchService.search({...params, ...pageable}).pipe(
-          finalize(() => {
-            this.pageLoading = false;
-          })
-        );
-      })
-    ).subscribe((page) => {
-      this.documentList?.loadPage(page);
-    })
-  }
-
-  doSearch(params: SearchParams) {
-    this.searchParams = params;
-    this.pageLoad.next({params: this.searchParams})
-  }
-
-  loadPage(pageable: Pageable) {
-    if (this.searchParams) {
-      this.pageLoad.next({params: this.searchParams, pageable})
-    }
-  }
+export class AccueilComponent {
 }
