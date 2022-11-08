b8e7850309e5a57b0e9860000530777551dcd2c6
Emanuel Guével <emanuel.guevel@sib.fr>
2022-11-08 15:42:27 +0100

    Marque blanche, layout sur l'accueil pour préparer l'ajout de la visionneuse PDF

--- src/app/marque-blanche/components/accueil/accueil.component.ts
+++ src/app/marque-blanche/components/accueil/accueil.component.ts
@@ -1,16 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnDestroy } from '@angular/core';
+import { Subject } from "rxjs";
+import { BreakpointObserver } from "@angular/cdk/layout";
+import { takeUntil } from "rxjs/operators";
+import { CommonModule } from "@angular/common";
 import { SearchListComponent } from "../search-list/search-list.component";
+
+const breakpointLarge = '(min-width: 960px)';
 
 @Component({
   selector: 'app-accueil',
   templateUrl: './accueil.component.html',
+  styleUrls: ['./accueil.component.scss'],
   standalone: true,
   imports: [
+    CommonModule,
     SearchListComponent
   ],
   host: {
     class: 'd-flex'
   }
 })
-export class AccueilComponent {
+export class AccueilComponent implements OnDestroy {
+  private destroyed = new Subject<void>();
+
+  private layoutChanges = inject(BreakpointObserver)
+    .observe([breakpointLarge])
+    .pipe(takeUntil(this.destroyed));
+
+  showPdfViewer = false;
+
+  constructor() {
+    this.layoutChanges.subscribe(state => {
+      this.showPdfViewer = state.breakpoints[breakpointLarge];
+    });
+  }
+
+  ngOnDestroy() {
+    this.destroyed.next();
+    this.destroyed.complete();
+  }
+
 }
