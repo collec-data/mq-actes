80e0facedeaad6593644ccfc30b53361136c3dfa
gautierr <romain.gautier@sib.fr>
2022-12-07 18:21:26 +0100

    Suppression mecanisme chargement iframe

--- src/app/app-routes.ts
+++ src/app/app-routes.ts
@@ -1,7 +1,6 @@
 import { Routes } from '@angular/router';
-import { IFrameRedirectVersMarqueBlancheGuard } from './iframe-redirect.guard';
 
-export const ACCUEIL_PATH = 'accueil';
+export const ACCUEIL_PATH = '';
 export const ACCUEIL_URL = `/${ACCUEIL_PATH}`;
 
 export const MARQUE_BLANCHE_PATH = 'marque-blanche';
@@ -10,11 +9,6 @@ export const MARQUE_BLANCHE_URL = `/${MARQUE_BLANCHE_PATH}`;
 export const routes: Routes = [
   {
     path: ACCUEIL_PATH,
-    loadChildren: () => import('./principal/principal-routes').then(m => m.routes),
-    canLoad: [IFrameRedirectVersMarqueBlancheGuard]
-  },
-  {
-    path: MARQUE_BLANCHE_PATH,
     loadChildren: () => import('./marque-blanche/marque-blanche-routes').then(m => m.routes)
   },
   {path: '**', redirectTo: ACCUEIL_PATH},
--- src/app/principal/principal-routes.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-import { AccueilComponent } from './components/accueil/accueil.component';
-
-export const routes: Routes = [
-  {path: '', component: AccueilComponent}
-];
